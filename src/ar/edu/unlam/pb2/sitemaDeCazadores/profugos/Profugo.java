06190680bf8cb70a0222a429612c5bda1e015fd0
giuflor <giulianafpachilla@gmail.com>
2025-06-30 15:26:06 -0300

    Test queSePuedaCrearUnProfugo

--- src/ar/edu/unlam/pb2/sitemaDeCazadores/profugos/Profugo.java
+++ src/ar/edu/unlam/pb2/sitemaDeCazadores/profugos/Profugo.java
@@ -1,5 +1,28 @@
 package ar.edu.unlam.pb2.sitemaDeCazadores.profugos;
 
 public class Profugo {
+	private String nombre;
+	private int inocencia;
+	private int habilidad;
+	private boolean nervioso;
+	
+	public Profugo (String nombre,int inocencia,int habilidad,boolean nervioso) {
+		this.nombre = nombre;
+		this.inocencia = inocencia;
+		this.habilidad = habilidad;
+		this.nervioso = nervioso;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public Integer getInocencia() {
+		return inocencia;
+	}
+	public Integer getHabilidad() {
+		return habilidad;
+	}
+	public boleean isNervioso() {
+		return nervioso;
+	}
 
 }
--- test/ar/edu/unlam/pb2/sitemaDeCazadores/ProfugoTest.java
+++ test/ar/edu/unlam/pb2/sitemaDeCazadores/ProfugoTest.java
@@ -1,5 +1,14 @@
 package ar.edu.unlam.pb2.sitemaDeCazadores;
 
 public class ProfugoTest {
+	@Test
+	public void queSePuedaCrearUnProfugo() {
+		Profugo p= new Profugo("Juan",30,75,true);
+		assertEquals("Juan",p.getNombre());
+		assertEquals(Integer.valueOf(30),p.getInocencia());
+		assertEquals(Integer.valueOf(75),p.getHabilidad());
+		assertTrue(p.isNevioso());
+	}
+	
 
 }
