c0f0d951e8d97b0a4a836725839c69a42cdb1e95
giuflor <giulianafpachilla@gmail.com>
2025-06-30 17:26:12 -0300

    test queSePuedaReducirLaInocenciaYHabilidad

--- test/ar/edu/unlam/pb2/sitemaDeCazadores/ProfugoTest.java
+++ test/ar/edu/unlam/pb2/sitemaDeCazadores/ProfugoTest.java
@@ -1,4 +1,9 @@
 package ar.edu.unlam.pb2.sitemaDeCazadores;
+import org.junit.Test;
+
+import ar.edu.unlam.pb2.sitemaDeCazadores.profugos.Profugo;
+
+import static org.junit.Assert.*;
 
 public class ProfugoTest {
 	@Test
@@ -9,6 +14,14 @@ public class ProfugoTest {
 		assertEquals(Integer.valueOf(75),p.getHabilidad());
 		assertTrue(p.isNervioso());
 		
+	}
+	@Test
+	public void queSePuedaReducirLaInocenciaYHabilidad() {
+		Profugo p= new Profugo("Ana",50,60,false);
+		p.reducirInocencia(10);
+		p.reducirHabilidad(15);
+		assertEquals(Integer.valueOf(40),p.getInocencia());
+		assertEquals(Integer.valueOf(45),p.getHabilidad());
 	}
 
 }
