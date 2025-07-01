43d9428b05990e39c83ce54289ed38e36e86da8f
giuflor <giulianafpachilla@gmail.com>
2025-06-30 23:10:00 -0300

    test queZonaPermitaCapturarYReducirProfugos

--- test/ar/edu/unlam/pb2/sitemaDeCazadores/ZonaTest.java
+++ test/ar/edu/unlam/pb2/sitemaDeCazadores/ZonaTest.java
@@ -4,18 +4,19 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import ar.edu.unlam.pb2.sitemaDeCazadores.profugos.IProfugo;
 import ar.edu.unlam.pb2.sitemaDeCazadores.profugos.Profugo;
 import ar.edu.unlam.pb2.sitemaDeCazadores.profugos.ProfugoEntrenado;
 import ar.edu.unlam.pb2.sitemaDeCazadores.zona.Zona;
 
-public class ZonaTest{
+public class ZonaTest {
 	@Test
 
 	public void queZonaPermitaCapturarYReducirProfugos() {
-		Zona zona=new Zona("Callejon");
-		CazadorRural cazador=new CazadorRural("Rural");
-		IProfugo base= new Profugo("Fuga",0,40,true);
-		ProfugoEntrenado entrenado=new ProfugoEntrenado(base);
+		Zona zona = new Zona("Callejon");
+		CazadorRural cazador = new CazadorRural("Rural");
+		IProfugo base = new Profugo("Fuga", 0, 40, true);
+		ProfugoEntrenado entrenado = new ProfugoEntrenado(base);
 		entrenado.entrenarEnArtesMarciales();
 		zona.agregarProfugo(entrenado);
 		zona.operarCaptura(cazador);
@@ -23,4 +24,3 @@ public class ZonaTest{
 		assertTrue(zona.getProfugos().isEmpty());
 	}
 }
-
