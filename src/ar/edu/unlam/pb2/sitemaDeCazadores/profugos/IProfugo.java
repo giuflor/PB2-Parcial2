3248fd1078bdbd73ba6a68d9d83b0612d95b5865
giuflor <giulianafpachilla@gmail.com>
2025-06-30 21:12:35 -0300

    interface IProfugo

---
+++ src/ar/edu/unlam/pb2/sitemaDeCazadores/profugos/IProfugo.java
@@ -0,0 +1,10 @@
+package ar.edu.unlam.pb2.sitemaDeCazadores.profugos;
+
+public interface IProfugo {
+	String getNombre();
+	Integer getInocencia();
+	Integer getHabilidad();
+	boolean isNervioso();
+}
+
+
