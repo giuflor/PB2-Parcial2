a47e75766361ebf3ce97596d345ece059750bff3
giuflor <giulianafpachilla@gmail.com>
2025-06-30 23:08:04 -0300

    clase zona

--- src/ar/edu/unlam/pb2/sitemaDeCazadores/zona/Zona.java
+++ src/ar/edu/unlam/pb2/sitemaDeCazadores/zona/Zona.java
@@ -11,31 +11,42 @@ import ar.edu.unlam.pb2.sitemaDeCazadores.profugos.IProfugo;
 
 public class Zona {
 	private String nombre;
-	private Set<IProfugo> profugos=new HashSet<>();
-	
-	public Zona (String nombre) {
-		this.nombre=nombre;
+	private Set<IProfugo> profugos = new HashSet<>();
+
+	public Zona(String nombre) {
+		this.nombre = nombre;
 	}
-	public String getNombre () {
+
+	public String getNombre() {
 		return nombre;
 	}
+
 	public void agregarProfugo(IProfugo p) {
 		profugos.add(p);
 	}
-	public Set <IProfugo> getProfugos(){
+
+	public Set<IProfugo> getProfugos() {
 		return profugos;
 	}
+
 	public void operarCaptura(Cazador cazador) {
-		List<IProfugo>copia= new ArrayList<>(profugos);
-		for(IProfugo p:copia) {
+		List<IProfugo> copia = new ArrayList<>(profugos);
+		for (IProfugo p : copia) {
 			try {
 				cazador.capturar(p);
 				profugos.remove(p);
-			}catch(ExceptionCapturaFallida e) {
-				//intimidacion ya aplicada
+			} catch (ExceptionCapturaFallida e) {
+				// intimidacion ya aplicada
 			}
 		}
 	}
-	
+
 }
 
+//Lo único que necesitás para que todo compile y funcione es:
+
+//Que tu clase CazadorRural extienda una clase Cazador que tenga:
+
+//Un método capturar(IProfugo p) que pueda lanzar ExceptionCapturaFallida.
+
+//Un método getCapturados() que devuelva una colección (por ejemplo Set<IProfugo>).
