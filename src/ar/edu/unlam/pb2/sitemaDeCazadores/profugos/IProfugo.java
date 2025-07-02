4a3d0edb925704ce82daffd04877d650ec8dadd3
giuflor <giulianafpachilla@gmail.com>
2025-07-01 21:01:50 -0300

    clase IProfugo refactorizada

--- src/ar/edu/unlam/pb2/sitemaDeCazadores/profugos/IProfugo.java
+++ src/ar/edu/unlam/pb2/sitemaDeCazadores/profugos/IProfugo.java
@@ -2,9 +2,16 @@ package ar.edu.unlam.pb2.sitemaDeCazadores.profugos;
 
 public interface IProfugo {
 	String getNombre();
+
 	Integer getInocencia();
+
 	Integer getHabilidad();
+
 	boolean isNervioso();
+
+	void reducirInocencia(int puntos);
+
+	void reducirHabilidad(int puntos);
+
+	void setIsNervioso(boolean nervioso);
 }
-
-
