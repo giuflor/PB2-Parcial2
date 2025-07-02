966057a265f881fd5952bfec1b9c4cd20c28019e
giuflor <giulianafpachilla@gmail.com>
2025-07-02 16:07:07 -0300

    test  queLaZonaActualiceElListadoDeProfugosDespuesDeVariasCapturas

--- test/ar/edu/unlam/pb2/sitemaDeCazadores/ZonaTest.java
+++ test/ar/edu/unlam/pb2/sitemaDeCazadores/ZonaTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import ar.edu.unlam.pb2.sitemaDeCazadores.cazadores.CazadorRural;
+import ar.edu.unlam.pb2.sitemaDeCazadores.cazadores.CazadorUrbano;
 import ar.edu.unlam.pb2.sitemaDeCazadores.profugos.Profugo;
 import ar.edu.unlam.pb2.sitemaDeCazadores.zona.Zona;
 
@@ -18,6 +19,25 @@ public class ZonaTest {
 		zona.agregarProfugo(profugo);
 		cazador.realizarCaptura(zona);
 		assertTrue(cazador.contieneCaptura(profugo));
+		assertTrue(zona.getProfugos().isEmpty());
+	}
+
+	@Test
+	public void queLaZonaActualiceElListadoDeProfugosDespuesDeVariasCapturas() {
+		Zona zona = new Zona("Zona Este");
+
+		Profugo p1 = new Profugo("Uno", 30, 40, false);
+		Profugo p2 = new Profugo("Dos", 30, 40, false);
+		Profugo p3 = new Profugo("Tres", 30, 40, false);
+
+		zona.agregarProfugo(p1);
+		zona.agregarProfugo(p2);
+		zona.agregarProfugo(p3);
+
+		CazadorUrbano cazador = new CazadorUrbano("Rick", 80);
+		cazador.realizarCaptura(zona);
+
+		// Después de la captura, la zona debe estar vacía
 		assertTrue(zona.getProfugos().isEmpty());
 	}
 }
