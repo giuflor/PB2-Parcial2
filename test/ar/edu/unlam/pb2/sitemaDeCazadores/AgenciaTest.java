95c96aaf7baba1ca7441e3c4c41e6f3c7f8a9522
giuflor <giulianafpachilla@gmail.com>
2025-07-02 10:55:11 -0300

    test queLaAgenciaObtengaTodosLosProfugosCapturados

--- test/ar/edu/unlam/pb2/sitemaDeCazadores/AgenciaTest.java
+++ test/ar/edu/unlam/pb2/sitemaDeCazadores/AgenciaTest.java
@@ -1,5 +1,54 @@
 package ar.edu.unlam.pb2.sitemaDeCazadores;
+package ar.edu.unlam.pb2.sitemaDeCazadores;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ar.edu.unlam.pb2.sitemaDeCazadores.agencia.Agencia;
+import ar.edu.unlam.pb2.sitemaDeCazadores.cazadores.CazadorRural;
+import ar.edu.unlam.pb2.sitemaDeCazadores.cazadores.CazadorSigiloso;
+import ar.edu.unlam.pb2.sitemaDeCazadores.cazadores.CazadorUrbano;
+import ar.edu.unlam.pb2.sitemaDeCazadores.profugos.Profugo;
+import ar.edu.unlam.pb2.sitemaDeCazadores.zona.Zona;
 
 public class AgenciaTest {
+
+	private Agencia agencia;
+	private CazadorUrbano cazadorUrbano;
+	private CazadorRural cazadorRural;
+	private CazadorSigiloso cazadorSigiloso;
+
+	@Before
+	public void setUp() {
+		agencia = new Agencia("Agencia Central");
+
+		cazadorUrbano = new CazadorUrbano("Rick", 80);
+		cazadorRural = new CazadorRural("Daryl", 70);
+		cazadorSigiloso = new CazadorSigiloso("Ghost", 90);
+
+		agencia.agregarCazador(cazadorUrbano);
+		agencia.agregarCazador(cazadorRural);
+		agencia.agregarCazador(cazadorSigiloso);
+	}
+
+	@Test
+	public void queLaAgenciaObtengaTodosLosProfugosCapturados() {
+		Profugo p1 = new Profugo("Smith", 50, 30, false);
+		Profugo p2 = new Profugo("Escapista", 40, 70, true);
+
+		Zona zona = new Zona("Ciudad");
+		zona.agregarProfugo(p1);
+		zona.agregarProfugo(p2);
+
+		cazadorUrbano.realizarCaptura(zona);
+		cazadorSigiloso.realizarCaptura(zona);
+
+		List<Profugo> capturados = agencia.obtenerTodosLosProfugosCapturados();
+		assertEquals(2, capturados.size());
+	}
 
 }
