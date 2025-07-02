a98caec0a7a9eb1141e09b6547b71a0188415efb
giuflor <giulianafpachilla@gmail.com>
2025-07-01 23:00:16 -0300

    test queLaHabilidadNoSupereElMaximoDe100ConArtesMarciales

--- test/ar/edu/unlam/pb2/sitemaDeCazadores/ProfugoEntrenadoTest.java
+++ test/ar/edu/unlam/pb2/sitemaDeCazadores/ProfugoEntrenadoTest.java
@@ -2,6 +2,7 @@ package ar.edu.unlam.pb2.sitemaDeCazadores.profugos;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -32,54 +33,55 @@ public class ProfugoEntrenadoTest {
 		// La habilidad se duplica con el entrenamiento y deberia seguir duplicada
 		assertEquals(Integer.valueOf(100), entrenado.getHabilidad());
 	}
+
 	@Test
 	public void queCazadorPuedaCapturarAProfugoEntrenadoConHabilidadDuplicada() {
-		Profugo base= new Profugo("Entrenado 2",40,20,false);
-		ProfugoEntrenado entrenado =new ProfugoEntrenado(base);
-		entrenado.entrenarEnArtesMarciales();//habilidad ahora es 40
-		Zona zona =new Zona("Zona B");
+		Profugo base = new Profugo("Entrenado 2", 40, 20, false);
+		ProfugoEntrenado entrenado = new ProfugoEntrenado(base);
+		entrenado.entrenarEnArtesMarciales();// habilidad ahora es 40
+		Zona zona = new Zona("Zona B");
 		zona.agregarProfugo(entrenado);
-		CazadorUrbano cazador=new CazadorUrbano("Urban Elite",60);
+		CazadorUrbano cazador = new CazadorUrbano("Urban Elite", 60);
 		cazador.realizarCaptura(zona);
-		//deberia capturarlo
+		// deberia capturarlo
 		assertTrue(cazador.getCapturados().contains(entrenado));
 		assertTrue(zona.getProfugos().isEmpty());
 	}
+
 	@Test
 	public void queCazadorNoCaptureNiIntimideSiInocenciaSuperaExperienciaAunConEntrenamiento() {
-		Profugo base= new Profugo("Inmune",90,10,true);
-		ProfugoEntrenado entrenado =new ProfugoEntrenado(base);
-		entrenado.recibirProteccionLegal();//No puede bajar de 40 inocencia
-		Zona zona =new Zona("Zona C");
+		Profugo base = new Profugo("Inmune", 90, 10, true);
+		ProfugoEntrenado entrenado = new ProfugoEntrenado(base);
+		entrenado.recibirProteccionLegal();// No puede bajar de 40 inocencia
+		Zona zona = new Zona("Zona C");
 		zona.agregarProfugo(entrenado);
-		CazadorSigiloso cazador =new CazadorSigiloso("Sigiloso Bajo",60);
+		CazadorSigiloso cazador = new CazadorSigiloso("Sigiloso Bajo", 60);
 		cazador.realizarCaptura(zona);
-		//no debe capturar ni intimidarlo
+		// no debe capturar ni intimidarlo
 		assertFalse(cazador.getCapturados().contains(entrenado));
 		assertTrue(zona.getProfugos().contains(entrenado));
-		
-	}
-	@Test
-	public void queProteccionLegalMantengaMinimoDeInocenciaAlIntimidar() {
-		Profugo base= new Profugo("Protegido",42,50,true);
-		ProfugoEntrenado entrenado=new ProfugoEntrenado(base);
-		entrenado.recibirProteccionLegal();//Inocencia no puede bajar de 40
-		Zona zona =new Zona("Zona D");
-		zona.agregarProfugo(entrenado);
-		CazadorRural cazador=new CazadorRural("Rural Bravo",80);
-		cazador.realizarCaptura(zona);
-		//La inocencia se reduce pero no puede bajar de 40
-		assertEquals(Integer.valueOf(40),entrenado.getInocencia());
-	}
-	@Test
-	public  void queLaHabilidadNoSupereElMaximoDe100ConArtesMarciales() {
-		Profugo base= new Profugo("Guerrero",30,60,false);
-		ProfugoEntrenado entrenado=new ProfugoEntrenado(base);
-		entrenado.entrenarEnArtesMarciales();//habilidad seria 120,pero debe ser como maximo 100
-		assertEquals(Integer.valueOf(100),entrenado.getHabilidad());
+
 	}
 
-		
-		
-	
+	@Test
+	public void queProteccionLegalMantengaMinimoDeInocenciaAlIntimidar() {
+		Profugo base = new Profugo("Protegido", 42, 50, true);
+		ProfugoEntrenado entrenado = new ProfugoEntrenado(base);
+		entrenado.recibirProteccionLegal();// Inocencia no puede bajar de 40
+		Zona zona = new Zona("Zona D");
+		zona.agregarProfugo(entrenado);
+		CazadorRural cazador = new CazadorRural("Rural Bravo", 80);
+		cazador.realizarCaptura(zona);
+		// La inocencia se reduce pero no puede bajar de 40
+		assertEquals(Integer.valueOf(40), entrenado.getInocencia());
+	}
+
+	@Test
+	public void queLaHabilidadNoSupereElMaximoDe100ConArtesMarciales() {
+		Profugo base = new Profugo("Guerrero", 30, 60, false);
+		ProfugoEntrenado entrenado = new ProfugoEntrenado(base);
+		entrenado.entrenarEnArtesMarciales();// habilidad seria 120,pero debe ser como maximo 100
+		assertEquals(Integer.valueOf(100), entrenado.getHabilidad());
+	}
+
 }
